d9573f39aad458e6598d3574c7d20f9ff85f6e3e
qegabbe <qegabbe@outlook.de>
2016-10-20 15:44:09 +0200

    Create install_ubuntu.sh

---
+++ install_ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#install nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install ASP Core
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+sudo apt-get update
+sudo apt-get install dotnet-dev-1.0.0-preview2-003131
+
+#install dotnet
+npm install -g yo generator-aspnetcore-spa
+npm install -g webpack
