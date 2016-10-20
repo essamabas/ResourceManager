139be0fc831b14d30aaad395772bef75fd97486d
Abas <Essam.Abas@getrag.com>
2016-10-20 17:21:06 +0200

    Change to Default

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -6,7 +6,9 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
-import { app_routing } from './app.routing';
+import { EmployeesModule } from './components/employees/employees.module';
+import { CoreModule }   from './components/core/core.module';
+import { SharedModule }   from './components/shared/shared.module';
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -15,11 +17,21 @@ import { app_routing } from './app.routing';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
+        EmployeesModule,
+        CoreModule,
+        SharedModule,
         HomeComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
-        app_routing
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'employees/:id', loadChildren: 'app/components/employee/employee.module'},
+            { path: '**', redirectTo: 'home' }
+        ])
     ]
 })
 export class AppModule {
