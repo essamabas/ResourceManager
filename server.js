939cfa4060cd1240d0b632106ec84fa714b42854
qegabbe <qegabbe@gmail.com>
2016-10-21 10:30:35 -0400

    Replace Customers with Employees

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+
 var express     = require('express'),
     bodyParser  = require('body-parser'),
     fs          = require('fs'),
     app         = express(),
-    customers   = JSON.parse(fs.readFileSync('data/customers.json', 'utf-8')),
-    orders      = JSON.parse(fs.readFileSync('data/orders.json', 'utf-8')),
+    employees   = JSON.parse(fs.readFileSync('data/employees.json', 'utf-8')),
+    tasks      = JSON.parse(fs.readFileSync('data/tasks.json', 'utf-8')),
     states      = JSON.parse(fs.readFileSync('data/states.json', 'utf-8'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,38 +17,38 @@ app.use('/node_modules', express.static(__dirname + '/node_modules'));
 //The src folder has our static resources (index.html, css, images)
 app.use(express.static(__dirname + '/src')); 
 
-app.get('/api/customers', (req, res) => {
-    res.json(customers);
+app.get('/api/employees', (req, res) => {
+    res.json(employees);
 });
 
-app.get('/api/customers/:id', (req, res) => {
-    let customerId = +req.params.id;
-    let selectedCustomer = {};
-    for (let customer of customers) {
-        if (customer.id === customerId) {
-           selectedCustomer = customer;
+app.get('/api/employees/:id', (req, res) => {
+    let employeeId = +req.params.id;
+    let selectedEmployee = {};
+    for (let employee of employees) {
+        if (employee.id === employeeId) {
+           selectedEmployee = employee;
            break;
         }
     }  
-    res.json(selectedCustomer);
+    res.json(selectedEmployee);
 });
 
-app.post('/api/customers', (req, res) => {
-    let postedCustomer = req.body;
-    let maxId = Math.max.apply(Math,customers.map((cust) => cust.id));
-    postedCustomer.id = ++maxId;
-    postedCustomer.gender = (postedCustomer.id % 2 === 0) ? 'female' : 'male';
-    customers.push(postedCustomer);
+app.post('/api/employees', (req, res) => {
+    let postedEmployee = req.body;
+    let maxId = Math.max.apply(Math,employees.map((cust) => cust.id));
+    postedEmployee.id = ++maxId;
+    postedEmployee.gender = (postedEmployee.id % 2 === 0) ? 'female' : 'male';
+    employees.push(postedEmployee);
     res.json({ status: true });
 });
 
-app.put('/api/customers/:id', (req, res) => {
-    let putCustomer = req.body;
+app.put('/api/employees/:id', (req, res) => {
+    let putEmployee = req.body;
     let id = +req.params.id;
     let status = false;
-    for (let i=0,len=customers.length;i<len;i++) {
-        if (customers[i].id === id) {
-            customers[i] = putCustomer;
+    for (let i=0,len=employees.length;i<len;i++) {
+        if (employees[i].id === id) {
+            employees[i] = putEmployee;
             status = true;
             break;
         }
@@ -55,26 +56,26 @@ app.put('/api/customers/:id', (req, res) => {
     res.json({ status: status });
 });
 
-app.delete('/api/customers/:id', function(req, res) {
-    let customerId = +req.params.id;
-    for (let i=0,len=customers.length;i<len;i++) {
-        if (customers[i].id === customerId) {
-           customers.splice(i,1);
+app.delete('/api/employees/:id', function(req, res) {
+    let employeeId = +req.params.id;
+    for (let i=0,len=employees.length;i<len;i++) {
+        if (employees[i].id === employeeId) {
+           employees.splice(i,1);
            break;
         }
     }  
     res.json({ status: true });
 });
 
-app.get('/api/orders', function(req, res) {
-    res.json(orders);
+app.get('/api/tasks', function(req, res) {
+    res.json(tasks);
 });
 
-app.get('/api/orders/:id', function(req, res) {
-    let customerId = +req.params.id;
-    for (let order of orders) {
-        if (order.customerId === customerId) {
-            return res.json([ order ]);
+app.get('/api/tasks/:id', function(req, res) {
+    let employeeId = +req.params.id;
+    for (let task of tasks) {
+        if (task.employeeId === employeeId) {
+            return res.json([ task ]);
         }
     }
     res.json([]);
@@ -96,7 +97,7 @@ console.log('Express listening on port 3000.');
 //Open browser
 var opn = require('opn');
 
-opn('http://localhost:3000').then(() => {
+opn('http://0.0.0.0:3000').then(() => {
     console.log('Browser closed.');
 });
 
